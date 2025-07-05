0037a8e204f8904a900610a75470ee04919290fe
izzybeetschen <114227602+izzybeetschen@users.noreply.github.com>
2025-07-05 16:02:10 +0100

    flower growing animation added

--- scripts/plant.gd
+++ scripts/plant.gd
@@ -3,7 +3,7 @@ extends AnimatedSprite2D
 @onready var plant: AnimatedSprite2D = $"."
 @onready var timer: Timer = $Timer
 
-var stages = ["sprout", "stem"]		# add flower
+var stages = ["sprout", "stem", "flower"]		# add flower
 var current_stage = 0
 
 # Called when the node enters the scene tree for the first time.
